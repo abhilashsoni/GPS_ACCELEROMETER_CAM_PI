2bb8b2fac4911129e258dc80df5b2c7964cb068f
ashwanikr17 <32015935+ashwanikr17@users.noreply.github.com>
2018-09-28 23:05:23 +0530

    Changing data collection rates

--- LOG/sensor_polling_multithreaded.py
+++ LOG/sensor_polling_multithreaded.py
@@ -177,7 +177,7 @@ def writeimage():
 		f.write("%s\n" %data)  
 		f.close()   
 		os.unlink(string1)
-		time.sleep(5)
+		time.sleep(10)
 def writepol():
 	global address_prefix,dbuffer
 	while True:
@@ -252,7 +252,7 @@ def writepol():
 		#fb1.post('Pol_data',data)
 		dbuffer = dbuffer[32:]
 		avpg=1
-		time.sleep(1)
+		time.sleep(0.5)
 #Beginning of the program
 IMU.detectIMU()
 IMU.initIMU()
