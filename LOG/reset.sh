87cee65a9a64a66abf88f5d1fc626387f735e2d2
Ashwani <ashwanikr17@gmail.com>
2018-11-10 12:00:18 +0530

    Changed to make a backup directory

--- LOG/reset.sh
+++ LOG/reset.sh
@@ -1,10 +1,7 @@
+rm -rf backup
 rm -f image/*
 rm -f bme/*
 rm -f poldata/*
 rm -f arduino/*
 rm -f gps/*
-touch image/.keep
-touch bme/.keep
-touch poldata/.keep
-touch arduino/.keep
-touch gps/.keep
+mkdir backup
