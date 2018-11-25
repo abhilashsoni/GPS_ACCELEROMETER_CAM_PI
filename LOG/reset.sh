1e608f12d7947a4142c1c60a8898cc8446a65d24
ashwanikr17 <32015935+ashwanikr17@users.noreply.github.com>
2018-11-25 17:16:45 +0530

    Corrected deletions

--- LOG/reset.sh
+++ LOG/reset.sh
@@ -1,12 +1,12 @@
-rm -rf backup
-rm -rf image/*
-rm -rf bme/*
-rm -rf poldata/*
-rm -rf arduino/*
-rm -rf gps/*
-mkdir backup
-mkdir backup/image
-mkdir backup/bme
-mkdir backup/poldata
-mkdir backup/arduino
-mkdir backup/gps
+#rm -rf backup
+rm -f image/*
+rm -f bme/*
+rm -f poldata/*
+rm -f arduino/*
+rm -f gps/*
+# mkdir backup
+# mkdir backup/image
+# mkdir backup/bme
+# mkdir backup/poldata
+# mkdir backup/arduino
+# mkdir backup/gps
